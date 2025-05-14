9fb659163b590a9584b7749cfbeb0ab24af6ade1
Niels Coopmans <nielscoopmans@gmail.com>
2025-05-14 19:18:28 +0200

    ping log

--- dashboard/src/Components/PingRpi.js
+++ dashboard/src/Components/PingRpi.js
@@ -3,11 +3,13 @@ import axios from "axios";
 const pingRpi = async (hostname) => {
     try {
         const response = await axios.get(`http://localhost:5000/ping/${hostname}`);
+        console.log(response);
         if (response.data.status === 'alive') {
             return 'working';
         } else {
             return 'deactivated';
         }
+
     } catch (err) {
         return 'faulty';
     }
